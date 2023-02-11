071d94a3625d84ffa3ffafb1773bd8776089a590
Levi Huddleston <levilhuddleston@gmail.com>
2023-02-11 13:44:05 -0800

    Adding some test utilities

--- utilities/runbook.sh
+++ utilities/runbook.sh
@@ -1,4 +1,5 @@
 python3 train.py --img 640 --batch 10 --epochs 15 --data ../data.yaml  --weights yolov5s.pt --name yolov5s_results  --cache
 python3 train.py --img 640 --batch 16 --epochs 3 --data ../data.yaml --weights yolov5s.pt
 python3 detect.py --weights runs/train/exp/weights/best.pt --source ../laver_cup_2017_edit.mp4 --view-img
-ython3 detect.py  --source ../laver_cup_2017_edit.mp4 --view-img               
+python3 detect.py  --source ../laver_cup_2017_edit.mp4 --view-img    
+  yolo task=detect mode=predict model=best.pt conf=0.50 source=videos/paris_masters_full_clip.mp4  show=true    
--- yolov8_model.py
+++ yolov8_model.py
@@ -4,5 +4,5 @@ from utilities.utilities import get_roboflow_data
 
 #dataset = get_roboflow_data()
 model = YOLO("yolov8n.pt")  # load a pretrained YOLOv8n model
-model.train(data=f'/Users/levihuddleston/Documents/Repos/yolo_tennis/data.yaml')  # train the model
+model.train(data='data.yaml')  # train the model
 model.export(format="torchscript")
