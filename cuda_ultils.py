c9fc9e2c4561da281fcc3b0026296571fe2ff20d
lhuddleston16 <51417024+lhuddleston16@users.noreply.github.com>
2023-02-24 08:42:15 -0800

    train new model

---
+++ cuda_ultils.py
@@ -0,0 +1,7 @@
+import torch
+import gc
+torch.cuda.empty_cache()
+
+
+
+print(torch.cuda.memory_summary(device=None, abbreviated=False))
--- utilities/runbook.sh
+++ utilities/runbook.sh
@@ -1,7 +1,12 @@
-python3 train.py --img 640 --batch 10 --epochs 15 --data ../data.yaml  --weights yolov5s.pt --name yolov5s_results  --cache
-python3 train.py --img 640 --batch 16 --epochs 3 --data ../data.yaml --weights yolov5s.pt
-python3 detect.py --weights runs/train/exp/weights/best.pt --source ../laver_cup_2017_edit.mp4 --view-img
-python3 detect.py  --source ../laver_cup_2017_edit.mp4 --view-img    
-yolo task=detect mode=predict model=best_02_12.pt conf=0.20 source=videos/clips/sample_atp_rallies/sample_atp_rallies_1.mp4 show=true    
-yolo task=detect mode=train  model=yolov8n.pt data=/Users/levihuddleston/Documents/Repos/yolo_tennis/data.yaml epochs=1 imgsz=640
+#MAC
+yolo task=detect mode=train  model=yolov8m.pt data=data.yaml  epochs=2 imgsz=1920 batch=2
+yolo task=detect mode=predict model=best_02_12.pt conf=0.20 source=videos/clips/sample_atp_rallies/sample_atp_rallies_1.mp4 show=true  
 python3 track.py --source ../videos/clips/sample_atp_rallies/sample_atp_rallies_7.mp4 --yolo-weights ../best_02_12.pt --tracking-method strongsort --save-vid
+
+
+
+
+# PC
+yolo task=detect mode=train  model=yolov8m.pt data=C:\Users\15094\Documents\GitHub\yolo_tennis\data.yaml  epochs=2 imgsz=1920 batch=2
+yolo task=detect mode=predict model=best_02_16.pt conf=0.50 source=videos\roland_garros_full_clip.mp4 show=true
+python track.py --source ..\videos\roland_garros_full_clip.mp4 --yolo-weights ..\best_02_16.pt --tracking-method strongsort --imgsz 1920  --max-det 3 --conf-thres .35 --show-vid
